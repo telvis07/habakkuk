cb891330f4697c623502db4ac92a1e07fa9e9e1c
telvis <telvis@web-1.(none)>
2014-03-09 22:13:56 -0400

    added nginx recipe

--- chef/cookbooks/habakkuk/recipes/web.rb
+++ chef/cookbooks/habakkuk/recipes/web.rb
@@ -4,3 +4,15 @@ cookbook_file "/etc/yum.repos.d/nginx.repo" do
   owner "root"
   group "root"
 end
+
+package "nginx" do
+    :upgrade
+end
+
+cookbook_file "/etc/nginx/sites-enabled/bakkify" do
+  source "nginx/bakkify"
+  mode 0640
+  owner "root"
+  group "root"
+  notifies :restart, resources(:service => "nginx")
+end
